bf4e6273e8278286f92cb564f65030c1eb3451c0
brakejl <brakejl@battlers.ab.edu>
2011-12-13 15:26:04 -0500

    added main class

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    private Main(){return;}        
+
+    public static void main(String[] args)
+    {
+        new SupportSystem(new TechSupportAnswers(), new InputReader(System.in)).start();
+    }
+}
